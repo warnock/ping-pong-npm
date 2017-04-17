37d137cbbfb60ebf0d314f1680fc5c43787b1d3f
Megan Warnock <megandwarnock@gmail.com>
2017-04-17 10:33:43 -0700

    add new form to index.html add gulp task concatInterface

---
+++ js/signup-interface.js
@@ -0,0 +1,8 @@
+$(dicynent).ready(function() {
+  $('#signup').submit(function(event){
+    event.preventDefault();
+    var email = $('#email').val();
+    $('#signup').hide();
+    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
+  });
+});
